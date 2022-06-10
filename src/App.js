fbb41850a57fc98f05567c000511738d191698a7
SaiKiran <saikiranchatlapalli84@gmail.com>
2022-06-11 01:25:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Forgot from "./pages/Forgot";
+import Reset from "./pages/Reset";
+import { ToastContainer } from "react-toastify";
+import CreateBlog from "./components/CreateBlog";
+import AllBlogs from "./components/AllBlogs";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
+import EditBlog from "./components/EditBlog";
+// import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
+  const token = useSelector((state) => state.token.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer
+        position="top-center"
+        theme={"dark"}
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Routes>
+        {/* Authenticated  Routes */}
+        <Route index element={token ? <Home /> : <Login />} />
+        <Route path="/create" element={token ? <CreateBlog /> : <Login />} />
+        <Route path="/allblog" element={token ? <AllBlogs /> : <Login />} />
+        <Route path="/:id" element={<EditBlog />} />
+
+        {/* Unauthenticated Routes */}
+        <Route path="/login" element={token ? <Home /> : <Login />} />
+        <Route path="/register" element={token ? <Home /> : <Register />} />
+        <Route path="/forgot" element={<Forgot />} />
+        <Route path="/reset" element={<Reset />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
